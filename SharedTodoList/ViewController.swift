44c93915a2ab5ae82acf2b32f0c911942994a3b3
Yongbin Yim <nowon28@gmail.com>
2019-10-15 22:30:11 -0700

    Initial Commit

---
+++ SharedTodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SharedTodoList
+//
+//  Created by Yim YongBin on 2019/10/15.
+//  Copyright © 2019 Yim YongBin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
